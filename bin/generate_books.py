e94edb28db1ab01f1b647f1a0086c6f7e2060fd5
oboreo <boris@southpawtech.com>
2014-10-17 00:48:10 -0400

    DOCS:    updated generate_books.py to copy the media files under build/media first    fixed some typo and missing png files

--- bin/generate_books.py
+++ bin/generate_books.py
@@ -9,6 +9,41 @@ books = [
     'tactic-setup'
 ]
 
+media_books = [
+    'tactic-developer',
+    'tactic-end-user',
+    'tactic-setup',
+    'tactic-quickstart',
+    'tactic-sys-admin'
+]
+
+plugin_books = [
+    'adobe-photoshop',
+    'cg-toolset',
+    'foundry-nuke',
+    'iridas-framecycler'
+]
+
+def copy_media(dir):
+    for root, dirs, files in os.walk(dir):
+        if 'media' in dirs:
+            path = '%s/media'%(root)
+            images = os.listdir(path)
+            for image in images:
+                image_path = '%s/%s' %(path, image)
+                if not os.path.exists( "../build/media/"):
+                    os.makedirs( "../build/media/")
+                shutil.copy(image_path, "../build/media/")
+
+for book in media_books:
+    dir = '../section/doc/%s'%book
+    copy_media(dir)
+
+for book in plugin_books:
+    dir = '../section/plugin/%s'%book
+    copy_media(dir)
+    
+    
 
 for book in books:
     cmd = 'asciidoc -b docbook ../book/%s.adoc' % book
@@ -23,7 +58,9 @@ for book in books:
     shutil.move("../book/%s.xml" % book, xml_path)
 
     #cmd = 'xsltproc --nonet /etc/asciidoc/docbook-xsl/chunked.xsl %s.xml' % book
-    cmd = 'a2x -D ../build -f chunked %s' % xml_path
+    
+
+    cmd = 'a2x -D ../build -f chunked  %s' % xml_path
     print
     print cmd
     print
@@ -40,10 +77,8 @@ for book in books:
     print cmd
     print
     os.system(cmd)
-
-
+    
     os.unlink(xml_path)
-
 
 
 
